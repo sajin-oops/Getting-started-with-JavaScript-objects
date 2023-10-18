9e939c199487d4fb14b2f998d61750e39e8c80d5
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2023-10-18 21:08:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// - Getting started with objects
+
+// - key value pair
+let item = {
+  name:'smartphone',
+  price:50000,
+  quantity:1
+}
+console.log(item)
+
+// output -{name: 'smartphone', price: 50000, quantity: 1}
